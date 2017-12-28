73905847dc022c36669c9b282ba18372f8d9c2e2
Simmo Saan <simmo.saan@gmail.com>
2017-12-28 16:00:43 +0200

    Add csv output of blocks and teams

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import block
 from pprint import pprint
 from z3 import *
 import itertools
+import csv
+import os
+from collections import defaultdict
 
 blocks = block.load_blocks("blocks.json")
 teams = 12
@@ -26,3 +29,35 @@ pprint(m)
 
 for _, block in blocks.items():
     block.print_debug(m)
+
+os.makedirs("out", exist_ok=True)
+
+def dump_csv_block(block, m):
+    with open("out/{}.csv".format(block.name), "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["start_time"] + [str(column) for column in range(block.columns)])
+
+        for row in block.grid_slots:
+            writer.writerow([row[0].start_time] + [str(m[slot.team_var]) for slot in row])
+
+def dump_csv_teams(blocks, m):
+    team_slots = defaultdict(dict)
+
+    for name, block in blocks.items():
+        for slot in block.slots:
+            team_slots[m[slot.team_var]][name] = slot
+
+    with open("out/teams.csv", "w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=["team"] + list(blocks.keys()))
+        writer.writeheader()
+
+        for team, slots in team_slots.items():
+            d = {"team": team}
+            d.update({name: slot.start_time for name, slot in slots.items()})
+            writer.writerow(d)
+
+
+for block in blocks.values():
+    dump_csv_block(block, m)
+
+dump_csv_teams(blocks, m)
